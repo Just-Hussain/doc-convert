7080d520d7942229203ad89a55e901d4bbe806da
Just-Hussain <1just.hussain@gmail.com>
2022-05-10 21:32:13 +0300

    backend stuff

--- backend/src/api/document/controllers/document.js
+++ backend/src/api/document/controllers/document.js
@@ -1,9 +1,44 @@
-'use strict';
+"use strict";
 
 /**
  *  document controller
  */
+const { createCoreController } = require("@strapi/strapi").factories;
+const { parseMultipartData, sanitize } = require("@strapi/utils");
 
-const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController(
+  "api::document.document",
+  ({ strapi }) => ({
+    async find(ctx) {
+      // some logic here
+      const { data, meta } = await super.find(ctx);
+      // some more logic
 
-module.exports = createCoreController('api::document.document');
+      return { data, meta };
+    },
+
+    async findOne(ctx) {
+      // some logic here
+      const response = await super.findOne(ctx);
+      // some more logic
+
+      return response;
+    },
+
+    async create(ctx) {
+      // some logic here
+      const response = await super.create(ctx);
+      // some more logic
+
+      return response;
+    },
+
+    async delete(ctx) {
+      // some logic here
+      const response = await super.delete(ctx);
+      // some more logic
+
+      return response;
+    },
+  })
+);
